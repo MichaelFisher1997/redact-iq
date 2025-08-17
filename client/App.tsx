ef833f8b3a430e588f475e00a40541e1de3e0007
MichaelFisher1997 <contact@michaelfisher.tech>
2025-08-17 17:24:35 +0100

    FEAT: added in dev popup

--- client/App.tsx
+++ client/App.tsx
@@ -7,27 +7,65 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "next-themes";
+import { useEffect, useState } from "react";
+import {
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogAction,
+} from "@/components/ui/alert-dialog";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </ThemeProvider>
-  </QueryClientProvider>
-);
+const App = () => {
+  const [showDevDialog, setShowDevDialog] = useState(false);
+
+  useEffect(() => {
+    const hasSeenDialog = localStorage.getItem("redactiq-dev-notice");
+    if (!hasSeenDialog) {
+      setShowDevDialog(true);
+    }
+  }, []);
+
+  const handleCloseDialog = () => {
+    localStorage.setItem("redactiq-dev-notice", "true");
+    setShowDevDialog(false);
+  };
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <AlertDialog open={showDevDialog} onOpenChange={setShowDevDialog}>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>🚧 In Development</AlertDialogTitle>
+                <AlertDialogDescription>
+                  This site is currently under development. Some features may not be fully functional yet.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogAction onClick={handleCloseDialog}>
+                Got it
+              </AlertDialogAction>
+            </AlertDialogContent>
+          </AlertDialog>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </TooltipProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+};
 
 createRoot(document.getElementById("root")!).render(<App />);
